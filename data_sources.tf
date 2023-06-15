bc1e332aa266ee270929f83dad3bf939f3c426d8
Ali Manouchehri <ali@manouchehri.ca>
2023-06-15 18:26:09 +0000

    SAS Viya Admin Group and the subnet for keyvault

--- data_sources.tf
+++ data_sources.tf
@@ -17,3 +17,15 @@ data "azurerm_route_table" "landing_zone_route_table" {
   name                = var.route_table_name
   resource_group_name = var.vnet_resource_group_name
 }
+
+#region Azure AD
+data "azuread_group" "group_sas_viya_admin" {
+  display_name = var.sas_viya_admin_group_name
+}
+
+# misc or mgmt subnet
+data "azurerm_subnet" "sn_misc" {
+  name                                           = var.subnet_names["misc"]
+  resource_group_name                            = data.azurerm_resource_group.landing_zone_utility_rg.name
+  virtual_network_name                           = data.azurerm_virtual_network.landing_zone_vnet.name
+}
