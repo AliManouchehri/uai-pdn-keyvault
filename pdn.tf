948a9e39431b43e8de638e46671230cd7f2b83a4
Ali Manouchehri <ali@manouchehri.ca>
2023-06-15 16:19:15 +0000

    Declared the pdns in vnet resource group

--- pdn.tf
+++ pdn.tf
@@ -7,24 +7,24 @@ provider "azurerm" {
 resource "azurerm_private_dns_zone" "pdns_keyvault" {
   provider            = azurerm.p
   name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.vnet_resource_group_name
 }
 
 resource "azurerm_private_dns_zone" "pdns_aks" {
   provider            = azurerm.p
   name                = "privatelink.canadacentral.azmk8s.io"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.vnet_resource_group_name
 }
 
 resource "azurerm_private_dns_zone" "flexpsql" {
   provider            = azurerm.p
   name                = "privatelink.postgres.database.azure.com"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.vnet_resource_group_name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "pdns_keyvault_vnet_link" {
   name                  = "pdns_keyvault_vnet_link"
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = var.vnet_resource_group_name
   private_dns_zone_name = "privatelink.vaultcore.azure.net"
   virtual_network_id    = data.azurerm_virtual_network.landing_zone_vnet.id
 
@@ -33,7 +33,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "pdns_keyvault_vnet_lin
 
 resource "azurerm_private_dns_zone_virtual_network_link" "pdns_aks_vnet_link" {
   name                  = "pdns_pdns_aks_vnet_link"
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = var.vnet_resource_group_name
   private_dns_zone_name = "privatelink.canadacentral.azmk8s.io"
   virtual_network_id    = data.azurerm_virtual_network.landing_zone_vnet.id
 
@@ -42,7 +42,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "pdns_aks_vnet_link" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "pdns_flexpsql_vnet_link" {
   name                  = "pdns_flexpsql_vnet_link"
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = var.vnet_resource_group_name
   private_dns_zone_name = "privatelink.postgres.database.azure.com"
   virtual_network_id    = data.azurerm_virtual_network.landing_zone_vnet.id
 
