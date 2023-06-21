d951cfbcd7d201206125e8b66dadc3ddab77959b
Ali Manouchehri <ali@manouchehri.ca>
2023-06-21 17:15:02 +0000

    Added azure_aks_admin

--- data_sources-aad.tf
+++ data_sources-aad.tf
@@ -3,7 +3,8 @@ locals {
   azure_cloud_admin          = "Azure - Cloud Admins"
   azure_sasviya_cloud_admin  = "Azure - SAS Viya Cloud Admin"
   azure_sasviya_reader       = "Azure - SAS Viya Reader"
-  azure_aks_user             = "Azure - AKS User"
+  azure_aks_user             = "Azure - SAS Viya AKS User"
+  azure_aks_admin            = "Azure - SAS Viya AKS Admin"
   azure_vm_operator          = "Azure - VM Operator"
 }
 
@@ -18,6 +19,9 @@ data "azuread_group" "azure_sasviya_reader" {
 }
 data "azuread_group" "azure_aks_user" {
   display_name = local.azure_aks_user
+}
+data "azuread_group" "azure_aks_admin" {
+  display_name = local.azure_aks_admin
 }
 data "azuread_group" "azure_vm_operator" {
   display_name = local.azure_vm_operator
